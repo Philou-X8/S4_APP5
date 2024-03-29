a88ea8af8cd9959a9d10c0e87676149f62302e78
Philou-X8 <pdurettesds@gmail.com>
2024-03-29 16:32:53 -0400

    Added syntax files

---
+++ src/app5/AST_Syntax/FeuilleAST.java
@@ -0,0 +1,35 @@
+package app5.AST_Syntax;
+
+/** @author Ahmed Khoumsi */
+
+/** Classe representant une feuille d'AST
+ */
+public class FeuilleAST extends ElemAST {
+
+  // Attribut(s)
+
+
+/**Constructeur pour l'initialisation d'attribut(s)
+ */
+  public FeuilleAST( ) {  // avec arguments
+    //
+  }
+
+
+  /** Evaluation de feuille d'AST
+   */
+  public int EvalAST( ) {
+      //
+
+      return 0; // TEMP
+  }
+
+
+  /** Lecture de noeud d'AST
+   */
+  public String LectAST( ) {
+      //
+      return ""; // temp
+  }
+
+}
---
+++ src/app5/AST_Syntax/NoeudAST.java
@@ -0,0 +1,37 @@
+package app5.AST_Syntax;
+
+/** @author Ahmed Khoumsi */
+
+/** Classe representant une feuille d'AST
+ */
+public class NoeudAST extends ElemAST {
+
+  // Attributs
+
+  /** Constructeur pour l'initialisation d'attributs
+   */
+  public NoeudAST( ) { // avec arguments
+    //
+  }
+
+ 
+  /** Evaluation de noeud d'AST
+   */
+  public int EvalAST( ) {
+     //
+
+    return 0; // TEMP
+  }
+
+
+  /** Lecture de noeud d'AST
+   */
+  public String LectAST( ) {
+     //
+
+    return ""; // temp
+  }
+
+}
+
+
